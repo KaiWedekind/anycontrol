3ef7fd5d035a3cee5e5b464291ae48dce4fca082
Kai Wedekind <wedekind.kai@googlemail.com>
2020-01-01 19:36:55 +0100

    feat(Command): Add commands that support keywords
    Add commands that support keywords
    
    #2

--- src/index.js
+++ src/index.js
@@ -33,19 +33,91 @@ const AnyControl = function AnyControl() {
       this.finalTranscript = this.finalTranscript.toLowerCase().trim();
       const commands = this.commands;
       Object.keys(commands).forEach((command) => {
-        if (this.finalTranscript.indexOf(command) > -1) {
-          if (this.finalTranscript[command.length] === undefined) {
-            if (this.DEBUG) {
-              console.info('calling command', command);
-            }
-            this.commands[command]();
-          } else if (this.finalTranscript[command.length] === ' ') {
-            const param = this.finalTranscript
-              .substring(command.length, this.finalTranscript.length).trim();
-            this.commands[command](param);
+        if (typeof this.commands[command] === 'function') {
+          if (this.finalTranscript.indexOf(command) > -1) {
+            if (this.finalTranscript[command.length] === undefined) {
+              if (this.DEBUG) {
+                console.info('calling command', command);
+              }
+              this.commands[command]();
+            } else if (this.finalTranscript[command.length] === ' ') {
+              const param = this.finalTranscript
+                .substring(command.length, this.finalTranscript.length).trim();
+              this.commands[command](param);
 
-            if (this.DEBUG) {
-              console.info('calling command', command, 'with param:', param);
+              if (this.DEBUG) {
+                console.info('calling command', command, 'with param:', param);
+              }
+            }
+          }
+        } else {
+          if (this.commands[command].fragments && typeof this.commands[command].callback === 'function') {
+            const snippets = this.commands[command].snippets;
+            const keywords = this.commands[command].keywords;
+            const startsWidthKeyword = this.commands[command].startsWidthKeyword;
+            const finalTranscript = this.finalTranscript;
+            let includes = true;
+            const context = {};
+
+            for (let k = 0; k < snippets.length; k += 1) {
+              const snippet = snippets[k].toLowerCase().trim();
+              if (finalTranscript.indexOf(snippet) === -1) {
+                includes = false;
+                break;
+              }
+            }
+
+            let transcriptFromTo;
+
+            if (includes) {
+              if (startsWidthKeyword) {
+                transcriptFromTo = finalTranscript
+                  .substring(finalTranscript.indexOf(snippets[0]));
+                const keyword = keywords[0];
+                const keywordValue = finalTranscript
+                  .substring(0, finalTranscript.indexOf(snippets[0]));
+
+                if (keyword && keywordValue) {
+                  context[keyword.replace('${', '').replace('}', '').trim()] = keywordValue.trim();
+                }
+
+                keywords.splice(0, 1);
+              } else {
+                transcriptFromTo = finalTranscript
+                  .substring(finalTranscript.indexOf(snippets[0]));
+              }
+
+              let transcript = transcriptFromTo;
+              for (let i = 0; i < snippets.length; i += 1) {
+                const snippet = snippets[i].trim();
+                const keyword = keywords[i];
+                transcript = transcript.trim();
+                transcript = transcript.replace(new RegExp(`^${snippet}`, 'gm'), '');
+                let keywordValue;
+                if (i + 1 < snippets.length) {
+                  keywordValue = transcript
+                    .substring(0, transcript.indexOf(snippets[i + 1]));
+                } else {
+                  keywordValue = transcript;
+                }
+
+                transcript = transcript.trim();
+                transcript = transcript
+                  .substring(transcript.indexOf(snippets[i + 1]));
+                transcript = transcript.trim();
+
+                if (keyword && keywordValue) {
+                  context[keyword.replace('${', '').replace('}', '').trim()] = keywordValue.trim();
+                }
+
+                context.transcript = this.finalTranscript;
+              }
+
+              this.commands[command].callback(context);
+
+              if (this.DEBUG) {
+                console.info(`calling command: "${command}" with params: `, context);
+              }
             }
           }
         }
@@ -101,7 +173,33 @@ AnyControl.prototype.addCommand = function addCommand(command, callback) {
     console.info('added command:', command);
   }
 
-  this.commands[command.toLowerCase()] = callback;
+  const keywords = command.match(/\${[a-zA-Z0-9_-]*}/gim);
+
+  if (keywords) {
+    const snippets = (command.split(/\${[a-zA-Z0-9_-]*}/gim) || []).filter(n => n).map(n => n.trim());
+    const startsWidthKeyword = command.match(/^\${[a-zA-Z0-9_-]*}/gim) || false;
+    const fragments = [];
+
+    for (let i = 0; i < keywords.length; i += 1) {
+      if (startsWidthKeyword) {
+        fragments.push(keywords[i].trim());
+        fragments.push(snippets[i].trim().toLowerCase());
+      } else {
+        fragments.push(snippets[i].trim().toLowerCase());
+        fragments.push(keywords[i].trim());
+      }
+    }
+
+    this.commands[command] = {
+      startsWidthKeyword,
+      snippets,
+      keywords,
+      fragments,
+      callback,
+    };
+  } else {
+    this.commands[command.toLowerCase()] = callback;
+  }
 };
 
 AnyControl.prototype.removeCommand = function removeCommand(command) {
